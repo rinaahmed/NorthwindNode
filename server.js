82aa7884904a7f15012e06ce6cac1df3042b5a6e
rinaacentosvmmean <rinaa@centosvm.mean>
2016-10-20 12:06:57 +0000

    some package installs

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var init = require('./config/init')(),
 	chalk = require('chalk');
 
 /**
- * Main application entry file.
+ * Main application entry file. ..
  * Please note that the order of loading is important.
  */
 
